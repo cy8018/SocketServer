65397dca33ea8239af2994c107b92601f45b06c7
Yu Cao <cy3118018@163.com>
2020-03-02 19:23:10 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace SocketServer
+{
+    class Program
+    {
+        private static string socketType = "SYNC";   // Socket connection type:  SYNC: Long connection ASYNC: Short connection
+        private static int serverPort = 20000;
+        private static string serverIP = "0.0.0.0";
+
+        private static void LoadConfig()
+        {
+            Configuration config = System.Configuration.ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            if (config.AppSettings.Settings["SocketType"].Value.Length > 0)
+            {
+                socketType = config.AppSettings.Settings["SocketType"].Value;
+            }
+
+            if (config.AppSettings.Settings["ServerListenIP"].Value.Length > 0)
+            {
+                serverIP = config.AppSettings.Settings["ServerListenIP"].Value;
+            }
+
+            string strServerPort = config.AppSettings.Settings["ServerListenPort"].Value;
+            if (strServerPort.Length > 0)
+            {
+                serverPort = int.Parse(strServerPort);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Process current = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+            foreach (Process process in processes)
+            {
+                if (process.Id != current.Id)
+                {
+                    if (process.MainModule.FileName == current.MainModule.FileName)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Application is already running, press any key to exit.");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+            }
+
+            LoadConfig();
+            SocketServer.Start(serverIP, serverPort);
+        }
+    }
+}
---
+++ Socket/SocketServer.cs
@@ -0,0 +1,157 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SocketServer
+{
+    public static class SocketServer
+    {
+        [DllImport("user32.dll", EntryPoint = "keybd_event", SetLastError = true)]
+        public static extern void keybd_event(Keys bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        private static ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private static byte[] result = new byte[1024];
+        
+        private static Socket serverSocket;
+        private static List<Socket> clientSockets = new List<Socket>();
+
+        public static void Start(string serverIP, int serverPort)
+        {
+            string log = string.Format("Service is now starting...");
+            Console.Clear();
+            Console.WriteLine(log);
+            Console.WriteLine();
+            logger.Warn(log);
+
+            IPAddress ip = IPAddress.Parse(serverIP);
+
+            try
+            {
+                serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                serverSocket.Bind(new IPEndPoint(ip, serverPort)); 
+                serverSocket.Listen(10);
+                log = string.Format("Listening at [{0}] successfully.", serverSocket.LocalEndPoint.ToString());
+
+                logger.Warn(log);
+                Console.WriteLine(log);
+
+                //通过Clientsoket发送数据
+                Thread myThread = new Thread(ListenClientConnect);
+                myThread.Start();
+            }
+            catch (Exception e)
+            {
+                log = string.Format("Exception when starting service：{0}", e.Message);
+
+                Console.WriteLine(log);
+                logger.Error(log);
+            }
+        }
+
+        private static void ListenClientConnect()
+        {
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                clientSockets.Add(clientSocket);
+                
+                string log = string.Format("Client [{0}] Connected.", clientSocket.RemoteEndPoint.ToString());
+                logger.Info(log);
+                if (logger.IsInfoEnabled || logger.IsDebugEnabled)
+                {
+                    Console.WriteLine(log);
+                }
+
+                Thread receiveThread = new Thread(ReceiveMessage);
+                receiveThread.Start(clientSocket);
+            }
+        }
+
+        private static void ReceiveMessage(object clientSocket)
+        {
+            Socket myClientSocket = (Socket)clientSocket;
+            while (true)
+            {
+                try
+                {
+                    //通过clientSocket接收数据
+                    int receiveNumber = myClientSocket.Receive(result);
+                    if(receiveNumber > 0)
+                    {
+                        string strContent = Encoding.ASCII.GetString(result, 0, receiveNumber);
+                        if (strContent.Length > 1)
+                        {
+                            string log = string.Format("Message [{1}] received from client [{0}]", myClientSocket.RemoteEndPoint.ToString(), Encoding.ASCII.GetString(result, 0, receiveNumber));
+
+                            logger.Debug(log);
+                            if (logger.IsDebugEnabled)
+                            {
+                                Console.WriteLine(log);
+                            }
+
+                            if (strContent.Contains("VolUp"))
+                            {
+                                keybd_event(Keys.Up, 0, 0, 0);
+                            }
+                            else if (strContent.Contains("VolDown"))
+                            {
+                                keybd_event(Keys.Down, 0, 0, 0);
+                            }
+                            else if (strContent.Contains("Next"))
+                            {
+                                keybd_event(Keys.PageDown, 0, 0, 0);
+                            }
+                            else if (strContent.Contains("Prev"))
+                            {
+                                keybd_event(Keys.PageUp, 0, 0, 0);
+                            }
+                            else if (strContent.Contains("PlayPause"))
+                            {
+                                keybd_event(Keys.Space, 0, 0, 0);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    logger.Error(ex.Message);
+
+                    if (myClientSocket != null && myClientSocket.Connected)
+                    {
+                        myClientSocket.Shutdown(SocketShutdown.Both);
+                    }                    
+                    myClientSocket.Close();
+                    break;
+                }
+                Thread.Sleep(20);
+            }
+        }
+
+        private static void SendMessage(string message)
+        {
+            foreach (Socket clientSocket in clientSockets)
+            {
+                if (clientSocket!=null&& clientSocket.Connected)
+                {
+                    clientSocket.Send(Encoding.ASCII.GetBytes(message));
+
+                    string log = string.Format("Send message [{1}] to client [{0}].", clientSocket.RemoteEndPoint.ToString(), message);
+
+                    logger.Debug(log);
+                    if (logger.IsDebugEnabled)
+                    {
+                        Console.WriteLine(log);
+                    }
+                }
+            }
+        }
+    }
+}
